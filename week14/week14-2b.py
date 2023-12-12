269e29ae6820beaae58a7663f6065b8435ce87cb
meteor0223 <liux05941@gmail.com>
2023-12-12 11:52:53 +0800

    week14

---
+++ week14/week14-2b.py
@@ -0,0 +1,4 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+#leedcode 1688
---
+++ week14/week14-6c.py
@@ -0,0 +1,6 @@
+a= input()
+b=int(a[0])*8
+c=int(a[1])*4
+d=int(a[2])*2
+e=int(a[3])*1
+print(b + c + d + e)
